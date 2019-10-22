94581a1c6b195f7f37fdc103f5b2b21390c55cae
Cardosaum <mcardosos99@gmail.com>
2019-10-22 16:53:21 -0300

    added packges and modules requirement - and startup.py

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 Custom scripts to save time
 
 ## Intended to work with [autokey](https://github.com/autokey/autokey)
+
+## Install
+* For the required packages, run this command:
+
+```
+< REQUIREMENTS.txt xargs sudo apt install -y
+```
+
+* For python specific packges, run this:
+```
+< PY_MODULES.txt xargs pip install --user
+```
---
+++ src/autokey/startup.py
@@ -0,0 +1,54 @@
+'''
+Script to run on every startup
+'''
+
+import platform, socket, re, uuid, json, psutil, pyautogui, os, pwd, subprocess, sys, mcs
+
+
+def get_system_info():
+    try:
+        info={}
+        info['platform']=platform.system()
+        info['platform-release']=platform.release()
+        info['platform-version']=platform.version()
+        info['architecture']=platform.machine()
+        info['hostname']=socket.gethostname()
+        info['ip-address']=socket.gethostbyname(socket.gethostname())
+        info['mac-address']=':'.join(re.findall('..', '%012x' % uuid.getnode()))
+        info['processor']=platform.processor()
+        info['ram']=str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"
+        return info
+    except Exception as e:
+        logging.exception(e)
+
+def get_username():
+    return pwd.getpwuid(os.getuid())[0]
+
+def cmd(command):
+    c = subprocess.run(command, shell=True, capture_output=True, text=True)
+    result = [c.stdout.strip(), c.stderr.strip()]
+    return result
+
+system_info = get_system_info()
+
+# Currently only support Linux
+if system_info['platform'] == 'Linux':
+
+    # Check in what computer the script is running
+    if system_info['hostname'] == 'uracila':
+        if get_username() == 'mcsouza':
+            mcs.waitUntilWindowActivate('Konsole')
+
+            # # set correct time
+            # mcs.waitUntilWindowActivate('Konsole')
+            # mcs.writeText('''sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"''')
+
+
+        else:
+        	pass
+
+    else:
+        pass
+
+else:
+    pyautogui.alert(title='Error', text="This program currently doesn't work on windows")
